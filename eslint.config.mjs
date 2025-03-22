1b442c569df816b40ad6e252c60c0e09b7ac5c1d
Your Name <your-email@example.com>
2025-03-22 02:11:35 +0100

    updated file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,12 +3,26 @@ import globals from "globals";
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-
+import next from "eslint-plugin-next";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    ignores: ["node_modules/", "dist/"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    plugins: {
+      js,
+      next,
+    },
+    extends: [
+      "js/recommended",
+      "next/core-web-vitals",
+      tseslint.configs.recommended,
+      pluginReact.configs.flat.recommended,
+    ],
+  },
 ]);
